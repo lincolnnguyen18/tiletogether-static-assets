b2103f73eab46c9e479f06bf8b704d00b1b488ea
XIAOTMA <49177628+maxt0214@users.noreply.github.com>
2022-10-26 15:54:54 -0400

    Fixed icons for file viewer Added FileViewer Added UI mockup readme Referenced Front End Models In Readme

--- README.md
+++ README.md
@@ -9,4 +9,6 @@
 6. Run `npm run test` to run tests
 
 ## Other notes
-* The TileTogether Static Assets (React app) are deployed at http://tiletogether-static-assets-bucket.s3-website-us-east-1.amazonaws.com/
+* The TileTogether Static Assets (React app) are deployed at http://tiletogether-static-assets-bucket.s3-website-us-east-1.
+amazonaws.com/
+* Icon CSS View: https://codepen.io/lincolnnguyen18/pen/rNKVaOz
--- src/features/FileViewer/FileViewer.js
+++ src/features/FileViewer/FileViewer.js
@@ -2,20 +2,44 @@
 import { css, jsx } from '@emotion/react';
 import { Fragment, useEffect } from 'react';
 import { Navbar } from '../Dashboard/Navbar/Navbar';
-import { Link, useLocation, useParams } from 'react-router-dom';
-import { Button } from '../../components/inputs/Button';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFileToView } from '../File/fileSlice';
+import { FileDescriber } from '../../components/FileDescriber';
 
 const fileViewerStyle = css`
-  background: red;
+  padding: 0 20px 8px 90px;
+  display: flex;
+`;
+
+const filePanelStyle = css`
+  flex: 3;
+  margin: 5px;
+  height: 100%;
+`;
+
+const fileRecomStyle = css`
+  flex: 1;
+  margin: 5px;
+  height: 100%;
+`;
+
+const canvasStyle = css`
+  width: 100%;
+  aspect-ratio: 270 / 170;
+  background: white;
+`;
+
+const imageStyle = css`
+  object-fit: contain;
+  width: 100%;
 `;
 
 export function FileViewer () {
   const { id } = useParams();
-  const location = useLocation();
+  // const location = useLocation();
   const dispatch = useDispatch();
-  const pathname = location.pathname;
+  // const pathname = location.pathname;
   const fileSlice = useSelector((state) => state.file);
   const userSlice = useSelector((state) => state.user);
   const file = fileSlice.file;
@@ -30,12 +54,26 @@ export function FileViewer () {
       <Navbar />
       {file && (
         <div css={fileViewerStyle}>
-          <h1>File Viewer</h1>
-          {file.authorUsername === username && (
-            <Link to='edit'>
-              <Button>Edit {pathname.split('/')[1].slice(0, -1)}</Button>
-            </Link>
-          )}
+          <div css={filePanelStyle}>
+            <div css={canvasStyle}>
+              <img src='/mock-data/file-image.png' css={imageStyle}/>
+            </div>
+            <FileDescriber
+              authorUserName={file.authorUserName}
+              filename={file.name}
+              views={1000}
+              publisDate={file.createdAt}
+              likes={file.likeCount}
+              description='Sample Description'
+              tags={file.tags}
+              type={file.type}
+              dimension={file.tileDimension}
+              liked={file.likes.some(l => l.username === username)}
+            />
+          </div>
+          <div css={fileRecomStyle}>
+            <h1>Recommendation</h1>
+          </div>
         </div>
       )}
     </Fragment>
