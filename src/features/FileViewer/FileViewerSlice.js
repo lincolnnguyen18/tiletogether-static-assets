58a6f25c09e7dd779a3c267994b73a7ff78ff8be
Lincoln Nguyen <letsmakeaworld@icloud.com>
2022-11-05 12:58:10 -0400

    Fix postComment

--- src/features/FileViewer/FileViewerSlice.js
+++ src/features/FileViewer/FileViewerSlice.js
@@ -3,9 +3,10 @@ import { apiClient } from '../../app/apiClient';
 
 const initialState = {};
 
-export const postComment = async ({ fileId, comment }) => {
+export const postComment = async ({ fileId, content }) => {
+  console.log(fileId, content);
   try {
-    const response = await apiClient.post(`/files/${fileId}/comment`, { content: comment });
+    const response = await apiClient.post(`/files/${fileId}/comment`, { content });
     return response.data;
   } catch (err) {
     throw new Error(JSON.stringify(err.response.data.error));
@@ -13,7 +14,7 @@ export const postComment = async ({ fileId, comment }) => {
 };
 
 const fileViewerSlice = createSlice({
-  name: 'file',
+  name: 'fileViewer',
   initialState,
   reducers: {},
   extraReducers: () => {},
