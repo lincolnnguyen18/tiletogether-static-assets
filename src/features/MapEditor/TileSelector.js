1a2c637b584db15a5e097f9f2ab6e02cc13cb01a
Lincoln Nguyen <letsmakeaworld@icloud.com>
2022-11-16 02:04:39 -0500

    Add click+drag select for tile selector

--- src/features/MapEditor/TileSelector.js
+++ src/features/MapEditor/TileSelector.js
@@ -18,17 +18,26 @@ export function TileSelector () {
   const { selectedTileset } = rightSidebarPrimitives;
   const tilesetCanvases = useSelector(selectTilesetCanvases);
   const file = useSelector(selectMapFile);
-  const [stageData, setStageData] = useState({ scale: 5, position: { x: 0, y: 0 } });
+  const [stageData, setStageData] = useState({ scale: 2, position: { x: 0, y: 0 } });
   const stageRef = useRef(null);
   const selectedTilesetCanvas = selectedTileset && tilesetCanvases[selectedTileset.file];
   const [gridLines, setGridLines] = useState(null);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [selectedTileRange, setSelectedTileRange] = useState(null);
 
   useEffect(() => {
     console.log('selectedTileset', selectedTileset);
+    // reset stage position and scale
+    setStageData({ scale: 2, position: { x: 0, y: 0 } });
+    setSelectedTileRange(null);
+    setSelectedTilesHighlight(null);
+    setSelectionDirection(null);
+    setPreviousSelectionDirection(null);
   }, [selectedTileset]);
 
   function handleStageMouseWheel (e) {
     e.evt.preventDefault();
+    if (!selectedTilesetCanvas) return;
     // if deltaY is abnormally large then ignore it
     if (Math.abs(e.evt.deltaY) > 1000) return;
 
@@ -118,6 +127,144 @@ export function TileSelector () {
     setGridLines(newGridLines);
   }, [selectedTilesetCanvas, stageData.scale]);
 
+  const [selectionDirection, setSelectionDirection] = useState(null);
+  const [previousSelectionDirection, setPreviousSelectionDirection] = useState(null);
+
+  // turn above into a reusable function
+  function getMouseTileCoords () {
+    // return index of tile that mouse is over, null if mouse is outside of tileset
+    const tileDimension = file.tileDimension;
+    const stageScale = stageData.scale;
+    const tilesetCanvas = selectedTilesetCanvas;
+    const tilesetCanvasWidth = tilesetCanvas.width;
+    const tilesetCanvasHeight = tilesetCanvas.height;
+    const mousePosition = stageRef.current.getPointerPosition();
+    const stagePosition = stageRef.current.position();
+    // const stageScale = stageRef.current.scaleX();
+    const relativeMousePos = {
+      x: Math.floor((mousePosition.x - stagePosition.x) / stageScale),
+      y: Math.floor((mousePosition.y - stagePosition.y) / stageScale),
+    };
+
+    // if clicked outside of tileset, return
+    if (relativeMousePos.x < 0 || relativeMousePos.x >= tilesetCanvasWidth) return;
+    if (relativeMousePos.y < 0 || relativeMousePos.y >= tilesetCanvasHeight) return;
+    // console.log('relativeMousePos', relativeMousePos);
+    // console.log('height', tilesetCanvasHeight);
+
+    // get index of tile that was clicked
+    // const tileIndex = Math.floor(relativeMousePos.x / tileDimension) + Math.floor(relativeMousePos.y / tileDimension) * (tilesetCanvasWidth / tileDimension);
+
+    const isPos = { x: true, y: true };
+
+    // also return x and y of tile in terms of number of tiles
+    let tileX = Math.floor(relativeMousePos.x / tileDimension);
+    if (selectedTileRange && selectedTileRange.start && selectedTileRange.start.tileX > tileX) {
+      isPos.x = false;
+    }
+    let tileY = Math.floor(relativeMousePos.y / tileDimension);
+    if (selectedTileRange && selectedTileRange.start && selectedTileRange.start.tileY > tileY) {
+      isPos.y = false;
+    }
+
+    if (selectionDirection) {
+      if (!selectionDirection.x) {
+        tileX -= 1;
+      }
+      if (!selectionDirection.x && previousSelectionDirection.x) {
+        const newSelectedTileRange = { ...selectedTileRange };
+        newSelectedTileRange.start.tileX += 1;
+        setSelectedTileRange(newSelectedTileRange);
+      } else if (previousSelectionDirection && selectionDirection.x && !previousSelectionDirection.x) {
+        const newSelectedTileRange = { ...selectedTileRange };
+        newSelectedTileRange.start.tileX -= 1;
+        setSelectedTileRange(newSelectedTileRange);
+      }
+      if (!selectionDirection.y) {
+        tileY -= 1;
+      }
+      if (!selectionDirection.y && previousSelectionDirection.y) {
+        const newSelectedTileRange = { ...selectedTileRange };
+        newSelectedTileRange.start.tileY += 1;
+        setSelectedTileRange(newSelectedTileRange);
+      } else if (previousSelectionDirection && selectionDirection.y && !previousSelectionDirection.y) {
+        const newSelectedTileRange = { ...selectedTileRange };
+        newSelectedTileRange.start.tileY -= 1;
+        setSelectedTileRange(newSelectedTileRange);
+      }
+    }
+
+    setPreviousSelectionDirection(selectionDirection);
+    setSelectionDirection(isPos);
+
+    // // update start's tileX
+    // const newSelectedTileRange = { ...selectedTileRange };
+    // newSelectedTileRange.start.tileX -= 1;
+    // setSelectedTileRange(newSelectedTileRange);
+
+    return { tileX, tileY };
+  }
+
+  // useEffect(() => {
+  //   if (_.isEqual(selectionDirection, previousSelectionDirection)) return;
+  //
+  //   console.log('selectionDirection', selectionDirection);
+  //   // console.log('previousSelectionDirection', previousSelectionDirection);
+  // }, [selectionDirection]);
+
+  function handleStageMouseDown () {
+    // console.log('stage mousedown', e);
+
+    const tileInfo = getMouseTileCoords();
+    if (!tileInfo) return;
+
+    setMouseDown(true);
+    setSelectedTileRange({ start: tileInfo, end: tileInfo });
+  }
+
+  function handleStageMouseMove () {
+    if (!mouseDown) return;
+    // console.log('stage mousemove', e);
+    const tileInfo = getMouseTileCoords();
+    // if outside of tileset, return
+    if (!tileInfo || !selectedTileRange) return;
+    setSelectedTileRange({ start: selectedTileRange.start, end: tileInfo });
+  }
+
+  function handleStageMouseUp () {
+    // console.log('stage mouseup', e);
+    setMouseDown(false);
+    setSelectedTileRange(null);
+    setSelectionDirection(null);
+    setPreviousSelectionDirection(null);
+  }
+
+  // function handleStageMouseLeave () {
+  //   // console.log('stage mouseleave', e);
+  //   setMouseDown(false);
+  //   setSelectedTileRange(null);
+  // }
+
+  const [selectedTilesHighlight, setSelectedTilesHighlight] = useState(null);
+
+  useEffect(() => {
+    // console.log('selectedTileRange', selectedTileRange);
+
+    // draw a blue transparent rectangle over the selected tiles
+    // use tileX and tileY multiplied by tileDimension to get x and y
+    if (selectedTileRange && selectedTileRange.start && selectedTileRange.end) {
+      setSelectedTilesHighlight(
+        <Rect
+          x={selectedTileRange.start.tileX * file.tileDimension}
+          y={selectedTileRange.start.tileY * file.tileDimension}
+          width={(selectedTileRange.end.tileX - selectedTileRange.start.tileX + 1) * file.tileDimension}
+          height={(selectedTileRange.end.tileY - selectedTileRange.start.tileY + 1) * file.tileDimension}
+          fill="rgba(0, 0, 255, 0.2)"
+        />,
+      );
+    }
+  }, [selectedTileRange]);
+
   return (
     <Stage
       width={254}
@@ -128,6 +275,10 @@ export function TileSelector () {
       y={stageData.position.y}
       scale={{ x: stageData.scale, y: stageData.scale }}
       onWheel={handleStageMouseWheel}
+      onMouseDown={handleStageMouseDown}
+      onMouseMove={handleStageMouseMove}
+      onMouseUp={handleStageMouseUp}
+      // onMouseLeave={handleStageMouseLeave}
     >
       <Layer imageSmoothingEnabled={false}>
         {selectedTilesetCanvas && file && (
@@ -138,6 +289,7 @@ export function TileSelector () {
           />
         )}
         <Image image={selectedTileset && tilesetCanvases[selectedTileset.file]} />
+        {selectedTilesHighlight}
         {gridLines}
       </Layer>
     </Stage>
