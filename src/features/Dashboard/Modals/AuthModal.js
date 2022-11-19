f55dfb0e0a07e2cb8676a0fa1195d5127fd91ab4
Vivian Lin <33815743+VivianLin61@users.noreply.github.com>
2022-11-19 13:13:57 -0500

    add reset password modals (#40)
    * add reset password modals
    
    * Tiny update
    
    Co-authored-by: vivilin <vivilin@cs.sunysb.edu>
    Co-authored-by: Lincoln Nguyen <letsmakeaworld@icloud.com>

--- src/features/Dashboard/Modals/AuthModal.js
+++ src/features/Dashboard/Modals/AuthModal.js
@@ -9,6 +9,7 @@ import { asyncGetUser, asyncPostUser, selectUserStatuses } from '../../User/user
 import { useLocation, useNavigate } from 'react-router-dom';
 import { modalBodyStyle } from '../../../components/Modal/Modal';
 import { asyncGetFiles } from '../../File/fileSlice';
+import { FlexColumn } from '../../../components/layout/FlexColumn';
 
 export function openAuthModal (dispatch, type) {
   dispatch(setModalReactElements({
@@ -26,6 +27,10 @@ export function AuthModalHeader ({ type }) {
       return <h1>Log in</h1>;
     case 'register':
       return <h1>Register an account</h1>;
+    case 'email':
+      return <h1>Enter email</h1>;
+    case 'password':
+      return <h1>Change Password</h1>;
     default:
       throw new Error('Invalid auth modal type', type);
   }
@@ -37,6 +42,13 @@ export function AuthModalBody ({ type }) {
   const location = useLocation();
   const userStatuses = useSelector(selectUserStatuses);
   const [errors, setErrors] = useState({});
+  const linkButtonStyle = css`
+    text-align: center;
+    color: blue;
+    cursor: pointer;
+    display: inline-block;
+
+`;
 
   async function onRegister (formData) {
     const res = await dispatch(asyncPostUser(formData));
@@ -61,6 +73,14 @@ export function AuthModalBody ({ type }) {
     }
   }
 
+  async function onEmail (formData) {
+    openAuthModal(dispatch, 'password');
+    return formData;
+  }
+
+  async function onChangePassword (formData) {
+    return formData;
+  }
   return (
     <form css={modalBodyStyle} onSubmit={(e) => {
       e.preventDefault();
@@ -70,18 +90,22 @@ export function AuthModalBody ({ type }) {
           return onLogin(formData);
         case 'register':
           return onRegister(formData);
+        case 'email':
+          return onEmail(formData);
+        case 'password':
+          return onChangePassword(formData);
         default:
           throw new Error('Invalid auth modal type', type);
       }
     }} noValidate>
-      <Textfield
+ { type !== 'password' && <Textfield
         label='Email'
         type='email'
         autoFocus
         style={whiteInputStyle}
         name='email'
         error={errors.email}
-      />
+      /> }
       {type === 'register' && (
         <Textfield
           label='Username'
@@ -91,14 +115,14 @@ export function AuthModalBody ({ type }) {
           error={errors.username}
         />
       )}
-      <Textfield
-        label='Password'
-        type='password'
-        style={whiteInputStyle}
-        name='password'
-        error={errors.password}
-      />
-      {type === 'register' && (
+      { (type === 'login' || type === 'register' || type === 'password') && <Textfield
+            label='Password'
+            type='password'
+            style={whiteInputStyle}
+            name='password'
+            error={errors.password}
+      /> }
+      {(type === 'register' || type === 'password') && (
         <Textfield
           label='Confirm password'
           type='password'
@@ -110,12 +134,34 @@ export function AuthModalBody ({ type }) {
       {errors.login && (
         <span css={css`color: red;`}>{errors.login}</span>
       )}
+
       <Button
         style={blackButtonStyle}
         disabled={userStatuses.postUser === 'pending' || userStatuses.getUser === 'pending'}
       >
-        <span>{type === 'login' ? 'Login' : 'Register'}</span>
+        {type === 'login' && <span>Login</span>}
+        {type === 'register' && <span>Register</span>}
+        {type === 'email' && <span>Send Email</span>}
+        {type === 'password' && <span>Change Password</span>}
+
       </Button>
+      {type === 'register' &&
+        <div>Already have an account? <span css={linkButtonStyle } onClick={() => openAuthModal(dispatch, 'login')}>Login</span>
+        </div>
+        }
+      {type === 'login' &&
+        <FlexColumn gap={4}>
+          <div>
+          <span css={linkButtonStyle} onClick={() => openAuthModal(dispatch, 'email')}>Forgot Password?</span>
+          </div>
+          <div>No account? <span css={linkButtonStyle} onClick={() => openAuthModal(dispatch, 'register')}>Create one</span>
+          </div>
+        </FlexColumn>
+      }
+      {type === 'email' &&
+        <div>Remember Your Password? <span css={linkButtonStyle} onClick={() => openAuthModal(dispatch, 'login')}>Login</span>
+        </div>
+        }
     </form>
   );
 }
